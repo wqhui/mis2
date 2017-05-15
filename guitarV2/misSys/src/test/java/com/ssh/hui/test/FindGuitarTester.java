5384e15ea96477834f2e5f022e7cbb0f75919b2d
吴清辉 <937871009@qq.com>
2017-05-15 15:59:48 +0800

    测试代码

--- guitarV2/misSys/src/test/java/com/ssh/hui/test/FindGuitarTester.java
+++ guitarV2/misSys/src/test/java/com/ssh/hui/test/FindGuitarTester.java
@@ -21,7 +21,7 @@ public class FindGuitarTester {
       
     
     GuitarSpec whatErinLikes = 
-      new GuitarSpec(Builder.BEIJING, "Stratocastor", 
+      new GuitarSpec(Builder.JIANGSU, "Stratocastor", 
                      Type.ELECTRIC, Wood.CAMPHOR, Wood.CAMPHOR);
     List matchingGuitars = inventory.search(whatErinLikes,allGuitars);
     if (!matchingGuitars.isEmpty()) {
@@ -43,29 +43,13 @@ public class FindGuitarTester {
 
   private static List<Guitar> initializeInventory() {
      List<Guitar> allGuitarList=new ArrayList<Guitar>();//手动添加的guitar列表
-     allGuitarList.add(new Guitar("11277", 3999.95,
-            new GuitarSpec(Builder.JIANGSU, "CJ", Type.ACOUSTIC,Wood.CAMPHOR, Wood.CAMPHOR)) 
+     allGuitarList.add(new Guitar("110", 110.0,
+            new GuitarSpec(Builder.BEIJING, "CJ", Type.ACOUSTIC,Wood.BRICH, Wood.BRICH)) 
       );
-     allGuitarList.add(new Guitar("V95693", 1499.95, 
-      new GuitarSpec(Builder.BEIJING, "Stratocastor", Type.ELECTRIC, Wood.CAMPHOR, Wood.CAMPHOR)));
+     allGuitarList.add(new Guitar("119", 119.0, 
+      new GuitarSpec(Builder.JIANGSU, "Stratocastor", Type.ELECTRIC, Wood.CAMPHOR, Wood.CAMPHOR)));
      allGuitarList.add(new Guitar("V9512", 1549.95, 
       new GuitarSpec(Builder.GUANGZHOU, "Stratocastor", Type.ELECTRIC,Wood.KOREANPINE, Wood.KOREANPINE)));
-     allGuitarList.add(new Guitar("122784", 5495.95, 
-      new GuitarSpec(Builder.BEIJING, "D-18", Type.ACOUSTIC,Wood.BRICH, Wood.KOREANPINE)));
-     allGuitarList.add(new Guitar("76531", 6295.95, 
-      new GuitarSpec(Builder.JIANGSU, "OM-28", Type.ACOUSTIC,Wood.KOREANPINE, Wood.BRICH)));
-     allGuitarList.add(new Guitar("70108276", 2295.95, 
-      new GuitarSpec(Builder.BEIJING, "Les Paul", Type.ELECTRIC,Wood.KOREANPINE, Wood.KOREANPINE)));
-     allGuitarList.add(new Guitar("82765501", 1890.95, 
-      new GuitarSpec(Builder.GUANGZHOU, "SG '61 Reissue", Type.ELECTRIC,  Wood.BRICH, Wood.KOREANPINE)));
-     allGuitarList.add(new Guitar("77023", 6275.95, 
-      new GuitarSpec(Builder.JIANGSU, "D-28", Type.ACOUSTIC,Wood.CAMPHOR, Wood.BRICH)));
-     allGuitarList.add(new Guitar("1092", 12995.95, 
-      new GuitarSpec(Builder.BEIJING, "SJ", Type.ACOUSTIC,Wood.CAMPHOR, Wood.BRICH)));
-     allGuitarList.add(new Guitar("566-62", 8999.95, 
-      new GuitarSpec(Builder.OTHER, "Cathedral", Type.ACOUSTIC, Wood.BRICH, Wood.CAMPHOR)));
-     allGuitarList.add(new Guitar("6 29584", 2100.95, 
-      new GuitarSpec(Builder.BEIJING, "Dave Navarro Signature", Type.ELECTRIC,  Wood.KOREANPINE, Wood.CAMPHOR)));
     return allGuitarList;
   }
 }
